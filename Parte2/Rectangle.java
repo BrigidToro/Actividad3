668dd2a9f3b3a9d494ff45092228c28880d4b8b5
BrigidToro <126936511+BrigidToro@users.noreply.github.com>
2023-04-17 20:46:32 -0500

    Add files via upload

---
+++ Parte2/Rectangle.java
@@ -0,0 +1,27 @@
+package com.mycompany.figurasgeometrica;
+public class Rectangle {
+    
+    private double base;
+    private double altura;
+    
+    public Rectangle(double base, double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    public void setBase(double base){
+        this.base = base;
+    }
+    
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    
+    public double calcularArea(){
+        return base * altura;
+    }
+    
+    public double calcularPerimetro(){
+        return 2 * (base + altura);
+    }
+}
